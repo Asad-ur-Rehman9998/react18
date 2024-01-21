31d22009b80350de906320d6b9f2f19f7a8830f1
Arslan Akmal <arxlan62@gmail.com>
2024-01-21 19:25:30 +0500

    icons added

--- src/component/SaleProducts.jsx
+++ src/component/SaleProducts.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React,{useState,useEffect} from 'react'
+import { AiFillAliwangwang } from "react-icons/ai";
 
 const SaleProducts = () => {
     const [products,setProducts]=useState([])
@@ -19,9 +20,10 @@ const SaleProducts = () => {
         products.map((item)=>{
             return(
                 <div class="card" style={{width:"18rem"}}>
+                    <marquee>Asdasdsadasd</marquee>
                 <img src={item.image} class="card-img-top w-50 h-50" alt="..."/>
                 <div class="card-body">
-                  <h5 class="card-title">{item.title}</h5>
+                  <h5 class="card-title"><AiFillAliwangwang style={{color:"green",fontSize:"50px"}} />{item.title}</h5>
                   <p class="card-text">{item.description}</p>
                   <a href="#" class="btn btn-primary">{item.price}</a>
                 </div>
